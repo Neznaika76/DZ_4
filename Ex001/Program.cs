29eb7f84f00c47efeaf1db1da59bbcd7d41fdd43
Pavel <n9032692530@gmail.com>
2022-09-01 21:25:34 +0300

    Ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[]array = new int[4];
+Random random = new Random();
+
+for (int i=0; i<array.Length; i++)
+    array[i]=random.Next(100);
+
+Console.WriteLine("[{0}]", string.Join(",", array));
+
+for (int i=0; i<array.Length; i++)
+    array[i]=array[i]*(-1);
+
+Console.WriteLine("[{0}]", string.Join(",", array));
